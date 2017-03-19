3000cff35b8df0ecbcb4aea5c16e8cfb3c8723e7
David Ackley <ackleyd1@msu.edu>
2017-03-19 01:01:23 -0400

    home, cuteness, and not found

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
 // Require Node's http module and assign it to a variable
 var http = require('http')
 
+var randomNumber = Math.Random()
+
 // Create a new server that just says "Hi!!" at every route
 var server = http.createServer(function (request, response) {
   if (request.url === '/') {
     response.end(
       '<h1>Home</h1>' +
-      '<img src="https://i.imgur.com/jKhQJVH.jpg" alt="Waving hi">'
+      '<p>Greetings Web Adventurer!</p>'
     )
-  } else if (request.url === '/about') {
-    response.end('<h1>About</h1>')
+  } else if (request.url === '/random-joke') {
+    response.end(
+      '<h1>Random Joke</h1>' +
+      '' +
+      '<a href="/">I wanna go home</a>'
+    )
+  } else if (request.url === '/cuteness') {
+    response.end(
+      '<h1>Cuteness</h1>' +
+      '<img src=http://www.awesomelycute.com/gallery/2011/09/awesomely-cute-animals-335.jpg alt="meditating turtle">' +
+      '<a href="/">I wanna go home</a>'
+    )
   } else {
-    response.end('<h1>Page Not Found</h1>')
+    response.end(
+      '<h1>Page Not Found</h1>' +
+      '<a href="/">Go Home url, you\'re drunk' +
+      '<p>The requested url: ' + response.url + ' was not found on this server</p>'
+    )
   }
 })
 
